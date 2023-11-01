006d4766812ff04b3b8c8476ea6c7bf22d11a8ff
xrojas06 <ximenarojas06@gmail.com>
2023-11-01 12:53:29 -0500

    js commit

---
+++ Assets/js/introduction.js
@@ -0,0 +1,28 @@
+/*
+ $name
+ _name
+ name
+ nombrePersona
+ let numero=1;
+ let nombre = 'Tatiana';
+
+
+ pi = 3.1416
+
+
+
+ const pi
+ console.log('el numero${num1} es muy bonito')
+
+
+ */
+
+let a = 5;
+let b= 10;
+
+console.log(`la suma es ${a+b}`)
+console.log('Hello world')
+
+
+
+/* funcion de suma*/
